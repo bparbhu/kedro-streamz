5106873564dcbe596403843ff29394baac1160be
Brian Parbhu <brian.parbhu@gmail.com>
2023-08-14 23:37:19 -0400

    adding pipeline resgistry changes for tsfresh pipeline

--- src/kedro_streamz/pipeline_registry.py
+++ src/kedro_streamz/pipeline_registry.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-
+from pipelines.tsfresh_features.pipeline import create_pipeline as tsfresh_pipeline
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -12,5 +12,11 @@ def register_pipelines() -> dict[str, Pipeline]:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
     pipelines = find_pipelines()
+
+    # Register tsfresh_features pipeline
+    pipelines["tsfresh_features"] = tsfresh_pipeline()
+
+    # If you want the default pipeline to run all discovered pipelines at once:
     pipelines["__default__"] = sum(pipelines.values())
+
     return pipelines
